fed8fbcd2d02b7715776ae1d9c2fb7d11deef77f
CristianCM17 <cristiandavidcardoso@gmail.com>
2023-07-25 19:11:25 -0600

    Terminando

---
+++ app/src/main/java/com/example/proyectoieca/Reglamento.kt
@@ -0,0 +1,11 @@
+package com.example.proyectoieca
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Reglamento : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_reglamento)
+    }
+}
--- app/src/main/java/com/example/proyectoieca/options.kt
+++ app/src/main/java/com/example/proyectoieca/options.kt
@@ -9,6 +9,8 @@ class options : AppCompatActivity() {
 
     lateinit var btnAcceso : Button
     lateinit var btnModificar: Button
+    lateinit var btnReglamento: Button
+    lateinit var btnExamen: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,6 +18,8 @@ class options : AppCompatActivity() {
 
         btnAcceso= findViewById(R.id.btnAcceso)
         btnModificar= findViewById(R.id.btnModificar)
+        btnExamen= findViewById(R.id.btnExamen)
+        btnReglamento= findViewById(R.id.btnReglamento)
 
         btnAcceso.setOnClickListener {
             val intent = Intent(this,Contenido::class.java)
@@ -27,6 +31,15 @@ class options : AppCompatActivity() {
             startActivity(intent)
         }
 
+        btnModificar.setOnClickListener {
+            val intent = Intent(this,Reglamento::class.java)
+            startActivity(intent)
+        }
+
+        btnModificar.setOnClickListener {
+            val intent = Intent("https://ieca.conectatalentomx.com/app/virtual-classroom/3?moduleId=24&resourceId=217")
+            startActivity(intent)
+        }
 
     }
 }
